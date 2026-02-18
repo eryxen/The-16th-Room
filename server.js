56417e6ff432cfc21819f77e452cbdcb658c28d8
eryxen <eryxen@github.com>
2026-02-18 16:48:04 +0800

    fix: change port to 8080 for Zeabur

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 // 中间件
 app.use(cors());
